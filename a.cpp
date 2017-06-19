f9fc61b10934aac6cc2b4ac0bb9b555cc27ab137
ak27061996 <akchaurasiya27061996@gmail.com>
2017-06-19 23:56:14 +0530

    Initial commit

---
+++ a.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+//using namespace std;
+#define f(g,g2,g3) g##g2##g3//## are add like   a##b means ab
+main()
+{
+	int sss=1212;
+int var12=100;
+printf("%d",f(s,s,s));
+}
+
+/*main()
+{
+int  i;
+ printf("%d",scanf("%d",&i)); // value 10 is given as input here
+}*/
+
+/*int  main()
+{
+int i=5;
+printf("%d",i++ + ++i);
+}*/
